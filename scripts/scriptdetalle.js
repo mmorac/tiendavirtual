3327f7c4f6fcab3c198812e7a22e18aa24a36a43
Mario Mora <marioemorac@gmail.com>
2022-09-22 21:26:22 -0600

    Imágenes dinámicas
    Agregando el mostrar las imágenes de detalle de manera dinámica

--- scripts/scriptdetalle.js
+++ scripts/scriptdetalle.js
@@ -3,7 +3,11 @@ var modelo = JSON.parse(localStorage.getItem(modeloDetalles));
 
 $(document).ready(function () {
     document.getElementById('titulo').innerHTML = "Nissan " + modeloDetalles;
-    document.getElementById('imgPrin').src = modelo.urlimagen;
+    document.getElementById('detalles-foto1').src = modelo.foto1;
+    document.getElementById('detalles-foto2').src = modelo.foto2;
+    document.getElementById('detalles-foto3').src = modelo.foto3;
+    document.getElementById('detalles-foto4').src = modelo.foto4;
+    document.getElementById('detalles-foto5').src = modelo.foto1;
     document.getElementById('detalles-modelo').innerHTML = modeloDetalles;
     document.getElementById('detalles-estilo').innerHTML = modelo.estilo;
     document.getElementById('detalles-cilindraje').innerHTML = modelo.cilindraje;
